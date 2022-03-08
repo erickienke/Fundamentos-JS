49722aa3d342e0907903702aa5dec8ee73ded9e2
Erick <erickf.ienke@gmail.com>
2022-03-08 10:12:32 -0300

    operadores e funções

---
+++ comparacoes.js
@@ -0,0 +1,19 @@
+// == comparação implícita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto)
+
+// typeof
+
+console.log(typeof numero);
+console.log(typeof texto)
+
+// == só compara valor
+// === compara valor e o tipo de dado
+
+// conversão explícita
+
+Number()
+String()
